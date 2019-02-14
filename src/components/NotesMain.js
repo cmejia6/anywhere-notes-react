8ccfa1f327d5af22bfde3af127195f99823c77c8
Alex <carlos.mejia1342@gmail.com>
2019-02-13 17:29:11 -0800

    First Commit

--- src/components/NotesMain.js
+++ src/components/NotesMain.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../styles/NotesMain.css';
 
-class App extends Component {
+class NotesMain extends Component {
   render() {
     return (
       <div className="App">
@@ -25,4 +25,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default NotesMain;
