5be30fd8a1acbbb74353b0860adac0a155973b43
Alex <carlos.mejia1342@gmail.com>
2019-02-13 23:25:44 -0800

    Revert "Merge branch 'master' of https://github.com/cmejia6/anywhere-notes-react"
    This reverts commit 4bf02446cc2edbce57e04683c9ac205b13923278, reversing
    changes made to c8db08539eb0d9362ba7461eb4c04d805bdf44e0.

---
+++ src/components/NotePage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/NotePage.css'
+
+const NotePage = (props) => {
+  return (
+    <div className="page-container">
+
+      <div className="input-field">
+        <textarea rows="8" className="a" type="text" placeholder="Write your notes here...">
+        </textarea>
+      </div>
+
+      <button className ="save btn btn-success">
+            Save
+        </button>
+
+    </div>
+  )
+}
+
+export default NotePage
